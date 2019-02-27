dd8b0cd503394f943568e8944705d261e9f15240
sorgelig <pour.garbage@gmail.com>
2019-02-28 03:48:24 +0800

    Update sys.

--- BK0011M.sv
+++ BK0011M.sv
@@ -3,7 +3,7 @@
 //
 //
 // BK0011M for MiSTer
-// (C) 2017,2018 Sorgelig
+// (C) 2017-2019 Sorgelig
 //
 // This source file and all other files in this project is free software: 
 // you can redistribute it and/or modify it under the terms of the 
@@ -105,10 +105,20 @@ module emu
 	input         UART_RXD,
 	output        UART_TXD,
 	output        UART_DTR,
-	input         UART_DSR
+	input         UART_DSR,
+
+	// Open-drain User port.
+	// 0 - D+/RX
+	// 1 - D-/TX
+	// 2..5 - USR1..USR4
+	// Set USER_OUT to 1 to read from USER_IN.
+	input   [5:0] USER_IN,
+	output  [5:0] USER_OUT,
+
+	input         OSD_STATUS
 );
 
-assign VGA_F1 = 0;
+assign USER_OUT = '1;
 assign {UART_RTS, UART_TXD, UART_DTR} = 0;
 assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
 
@@ -573,6 +583,7 @@ wire [15:0] vram_data;
 
 wire  [1:0] scale = status[8:7];
 assign VGA_SL = {scale == 3, scale == 2};
+assign VGA_F1 = 0;
 
 video video
 (
